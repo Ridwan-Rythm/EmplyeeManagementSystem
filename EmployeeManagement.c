8da2e7afe1c3966e879ac80cf4ba4e5be6249daf
Shams Kabir <shamskbr27@gmail.com>
2025-07-09 22:48:19 +0600

    Quick Update

---
+++ EmployeeManagement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int id;
+    char name[50];
+    char position[30];
+    float salary;
+    int attendanceDays;
+    float bonus;
+    float deduction;
+} Employee;
+
+int main()
+{
+    return 0;
+}
